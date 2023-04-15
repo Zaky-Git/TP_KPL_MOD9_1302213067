e93a085ed146b58c8bdb5093ef70c2bc25cf318e
MUHAMMAD ZAKY FATHURAHIM <mzakyf5503@gmail.com>
2023-04-15 21:44:41 +0700

    menambahkan API mahasiswa

---
+++ tpmodul9_1302213067/Mahasiswa.cs
@@ -0,0 +1,10 @@
+﻿namespace tpmodul9_1302213067
+{
+    public class Mahasiswa
+    {
+        public string Nama { get; private set; }
+        public string Nim { get; private set; }
+
+        public Mahasiswa(string nama, string nim) { this.Nama = nama; this.Nim = nim; }
+    }
+}
